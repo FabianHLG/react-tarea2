c54315cf35f4bf31f68b74c184bc464823ae3d77
fabia <fabia@DESKTOP-CFIHJOU>
2024-10-11 11:20:56 -0600

    haciendo pruebas

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Empecemos a crear una aplicacion web con api, perfecto.
+          Empecemos a crear una aplicacion web con api, perfecto, actualizando.
         </p>
         <a
           className="App-link"
